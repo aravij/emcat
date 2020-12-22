20fb1a300a81be7685a75dbec8c5a340ce5f0e62
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-12-22 12:49:40 +0300

    Make single method processing a separate subcommand

--- src/main/java/com/github/emcat/Emcat.java
+++ src/main/java/com/github/emcat/Emcat.java
@@ -4,25 +4,21 @@ import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 
-import java.util.concurrent.Callable;
-
 import com.github.emcat.metric_calculators.MethodDescriptor;
 import com.github.emcat.metric_calculators.MethodMetricsAggregator;
 import com.github.emcat.metric_calculators.MethodMetricsData;
 
-@Command(name = "emcat", description = "Calculates given NCSS and cyclomatic complexity of a given method.", version = "1.0")
-public class Emcat implements Callable<Integer> {
-    @Option(names = {"-f", "--file"}, required = true, description = "File path to Java source code to analyse")
-    private transient String sourceCodeFilePath;
-
-    @Option(names = {"-c", "--class_name"}, required = true, description = "Name of the class to analyse")
-    private transient String className;
-
-    @Option(names = {"-m", "--method_name"}, required = true, description = "Method name to analyse")
-    private transient String methodName;
-
-    @Override
-    public Integer call() {
+@Command(name = "emcat", mixinStandardHelpOptions = true, description = "Calculates NCSS and Cyclomatic complexity of Java methods.", version = "1.0")
+public class Emcat {
+    @Command(mixinStandardHelpOptions = true)
+    public int single(
+        @Option(names = {"-f", "--file"}, required = true, description = "File path to Java source code to analyse")
+        final String sourceCodeFilePath,
+        @Option(names = {"-c", "--class_name"}, required = true, description = "Name of the class to analyse")
+        final String className,
+        @Option(names = {"-m", "--method_name"}, required = true, description = "Method name to analyse")
+        final String methodName
+    ) {
         final MethodMetricsAggregator methodMetricsAggregator = new MethodMetricsAggregator();
         final MethodDescriptor methodDescriptor = new MethodDescriptor(sourceCodeFilePath, className, methodName);
         final MethodMetricsData methodMetrics = methodMetricsAggregator.calculateSingleMethodMetrics(methodDescriptor);
@@ -43,7 +39,8 @@ public class Emcat implements Callable<Integer> {
         return exitCode;
     }
 
+    @SuppressWarnings("PMD.DoNotCallSystemExit")
     public static void main(final String... args) {
-        System.exit(new CommandLine(new Emcat()).execute(args));//NOPMD
+        System.exit(new CommandLine(new Emcat()).execute(args));
     }
 }
